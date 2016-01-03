0a1246cdccd47b65ec668c737ec4c2eb320fe8ee
Jared King <j@jaredtking.com>
2016-01-03 17:56:47 -0600

    test php7

--- src/models/SocialMediaProfile.php
+++ src/models/SocialMediaProfile.php
@@ -2,8 +2,8 @@
 
 namespace app\social\models;
 
-use infuse\Model;
-use infuse\Utility as U;
+use Infuse\Model;
+use Infuse\Utility as U;
 
 abstract class SocialMediaProfile extends Model
 {
@@ -54,7 +54,7 @@ abstract class SocialMediaProfile extends Model
 
     /**
      * Gets the property name that matches this profile's ID off the User object
-     * i.e. twitter_id, facebook_id, instagram_id
+     * i.e. twitter_id, facebook_id, instagram_id.
      *
      * @return string property name
      */
@@ -62,7 +62,7 @@ abstract class SocialMediaProfile extends Model
 
     /**
      * Map of API properties that correspond with profile properties
-     * [ 'model_property' => 'api_property' ]
+     * [ 'model_property' => 'api_property' ].
      *
      * @return array
      */
@@ -70,7 +70,7 @@ abstract class SocialMediaProfile extends Model
 
     /**
      * Gets the number of days until the profile is considered stale and
-     * must be refreshed from the API
+     * must be refreshed from the API.
      *
      * @return int days
      */
@@ -78,21 +78,21 @@ abstract class SocialMediaProfile extends Model
 
     /**
      * Gets the number of profiles to refresh at once in order to preven bumping
-     * into rate limiting or running forever
+     * into rate limiting or running forever.
      *
      * @return int
      */
     abstract public function numProfilesToRefresh();
 
     /**
-     * Gets the URL of the profile
+     * Gets the URL of the profile.
      *
-     * @return boolean
+     * @return bool
      */
     abstract public function url();
 
     /**
-     * Generates the URL for the profile's picture
+     * Generates the URL for the profile's picture.
      *
      * @param int $size size of the picture (it is square, usually)
      *
@@ -101,25 +101,25 @@ abstract class SocialMediaProfile extends Model
     abstract public function profilePicture($size = 80);
 
     /**
-     * Checks if InspireVive is authenticated for the profile
+     * Checks if InspireVive is authenticated for the profile.
      *
-     * @return boolean
+     * @return bool
      */
     abstract public function isLoggedIn();
 
     /**
-     * Fetches the profile from the API
+     * Fetches the profile from the API.
      *
      * @return array|false
      */
     abstract public function getProfileFromApi();
 
     /**
-     * Refreshes this profile using the API of the social network
+     * Refreshes this profile using the API of the social network.
      *
      * @param array $userProfile optional user profile
      *
-     * @return boolean
+     * @return bool
      */
     public function refreshProfile(array $userProfile = [])
     {
@@ -142,9 +142,9 @@ abstract class SocialMediaProfile extends Model
 
     /**
      * Refreshes stale twitter profiles. One call currently only refreshes
-     * 180 profiles to minimize bumping into twitter's rate limiting
+     * 180 profiles to minimize bumping into twitter's rate limiting.
      *
-     * @return boolean
+     * @return bool
      */
     public static function refreshProfiles()
     {
@@ -168,7 +168,7 @@ abstract class SocialMediaProfile extends Model
 
     /**
      * Maps the properties of the user profile from the API
-     * to the properties in our model
+     * to the properties in our model.
      *
      * @param array $user_profile user profile from API
      *
